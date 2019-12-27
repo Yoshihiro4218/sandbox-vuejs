93a9ace2cedc52f9673fe226ce409376737adae3
kadono <y.kadono4218@gmail.com>
2019-12-27 11:26:25 +0900

    途中

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
-<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+    <!--    <img alt="Vue logo" src="./assets/logo.png">-->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
 
     <p>{{message}}</p>
     <button v-on:click="show=!show">BUTTON</button>
-    <p v-if="show">Pokemon!!</p>
+    <transition>
+      <p v-if="show">Pokemon!!</p>
+    </transition>
 
   </div>
 </template>
@@ -18,11 +20,24 @@
     components: {
       HelloWorld
     },
+    // アプリケーションで使用するデータ。オブジェクトや配列も可能。
     data() {
       return {
         message: 'Fukuoka!',
         show: true
       }
+    },
+    // 算出プロパティ。dataと似たように扱うことの出来る、関数によって算出されたデータ
+    computed: {
+      computedMessage: function () {
+        //  処理した結果をデータとして返す
+        return this.message + '!!!!!(ﾆｯｺﾘ'
+      }
+    },
+    //  ライフサイクルフック
+    mounted() {
+      //  このインスタンスの作成 & 初期化が終わったらすぐ
+      this.message += '最高!CREATED';
     }
   }
 </script>
@@ -35,5 +50,13 @@
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+  }
+
+  .v-enter-active, .v-leave-active {
+    transition: opacity 1s;
+  }
+
+  .v-enter, v-leave-to {
+    opacity: 0;
   }
 </style>
