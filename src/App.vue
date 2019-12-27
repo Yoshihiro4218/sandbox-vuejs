8de25a68fd647f9f6f056519577f026e80c3632a
kadono <y.kadono4218@gmail.com>
2019-12-27 11:43:19 +0900

    途中

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,14 @@
     <HelloWorld msg="Welcome to Your Vue.js App"/>
 
     <p>{{message}}</p>
+    <p>{{message2.morning}}</p>
+    <p>{{message2.day.length}}</p>
+    <p>{{bankList}}</p>
+    <p>{{bankList[0]}}</p>
+    <button v-on:click="countUp">Bank!</button>
+    <p>{{bankList[num]}}</p>
     <button v-on:click="show=!show">BUTTON</button>
+    <!--    <button v-on:click="computedMessage">BUTTON2</button>-->
     <transition>
       <p v-if="show">Pokemon!!</p>
     </transition>
@@ -24,7 +31,15 @@
     data() {
       return {
         message: 'Fukuoka!',
-        show: true
+        show: true,
+        // 内容が決まっていない場合でも初期値や空データとして定義しておく必要がある
+        error: null,
+        message2: {
+          morning: 'おはよう！',
+          day: 'こんにちは！'
+        },
+        bankList: ['福岡銀行', '親和銀行', '熊本銀行', '十八銀行'],
+        num: 0
       }
     },
     // 算出プロパティ。dataと似たように扱うことの出来る、関数によって算出されたデータ
@@ -35,9 +50,18 @@
       }
     },
     //  ライフサイクルフック
-    mounted() {
+    created() {
       //  このインスタンスの作成 & 初期化が終わったらすぐ
-      this.message += '最高!CREATED';
+      this.message += ' + 最高! CREATED';
+    },
+    methods: {
+      countUp() {
+        if (this.num >= 3) {
+          this.num = 0;
+        } else {
+          this.num++;
+        }
+      }
     }
   }
 </script>
